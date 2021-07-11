25ff29d5b5255d7eca4ff3213b31b9ba0edb74b8
Nikita-GIF110 <65663981+Nikita-GIF110@users.noreply.github.com>
2021-07-11 23:01:28 +0300

    Add files via upload

--- scripts/dist/main.dev.js
+++ scripts/dist/main.dev.js
@@ -1,38 +1,51 @@
 "use strict";
 
-var btnBurger = document.querySelector('.menu_btn');
-var content = document.querySelector('.content');
-var articleWrap = document.querySelector('.article_wrap');
-var articleReserve = document.querySelector('.article_reserve');
-var articleMap = document.querySelector('.article_map');
-var article = document.querySelector('.article');
-var news = document.querySelector('.news');
-btnBurger.addEventListener('click', function (event) {
-  btnBurger.classList.toggle('menu_btn-active');
-  article.classList.toggle('article-active');
-  content.classList.toggle('content-active');
-  articleReserve.classList.toggle('article_reserve-active');
-  news.classList.toggle('news-active');
-  event.preventDefault();
-});
-var btns = document.querySelectorAll("*[data-modal_btn]");
-var close = document.querySelectorAll('.news_modal_window_close');
-var modalOverlay = document.querySelectorAll('.news_modal');
-
-var _loop = function _loop(i) {
-  btns[i].addEventListener('click', function (e) {
-    var name = btns[i].getAttribute('data-modal_btn');
-    var modal = document.querySelector("[data-modal_window = '" + name + "']");
-    modal.classList.add('show');
-
-    for (var _i = 0; _i < close.length; _i++) {
-      close[_i].addEventListener('click', function () {
-        modal.classList.remove('show');
-      });
-    }
+document.addEventListener('DOMContentLoaded', function () {
+  var btnBurger = document.querySelector('.menu_btn');
+  var content = document.querySelector('.content');
+  var articleReserve = document.querySelector('.article_reserve');
+  var article = document.querySelector('.article');
+  var news = document.querySelector('.news');
+  btnBurger.addEventListener('click', function (event) {
+    btnBurger.classList.toggle('menu_btn-active');
+    article.classList.toggle('article-active');
+    content.classList.toggle('content-active');
+    articleReserve.classList.toggle('article_reserve-active');
+    news.classList.toggle('news-active');
+    event.preventDefault();
   });
-};
+  var modalBtns = document.querySelectorAll("#js-open-modal");
+  var modalOverlay = document.querySelectorAll("#js-overlay-modal");
+  var modalClose = document.querySelectorAll("#js-modal-close");
 
-for (var i = 0; i < btns.length; i++) {
-  _loop(i);
-}
+  var _loop = function _loop(i) {
+    modalBtns[i].addEventListener('click', function (e) {
+      var parth = modalBtns[i].getAttribute('data-parth');
+      var modal = document.querySelector("[data-target=\"".concat(parth, "\"]"));
+      modal.classList.add('show');
+      e.preventDefault();
+
+      for (var _i2 = 0; _i2 < modalClose.length; _i2++) {
+        modalClose[_i2].addEventListener('click', function () {
+          modal.classList.remove('show');
+        });
+      }
+    });
+  };
+
+  for (var i = 0; i < modalBtns.length; i++) {
+    _loop(i);
+  }
+
+  var _loop2 = function _loop2(_i) {
+    modalOverlay[_i].addEventListener('click', function (e) {
+      if (e.target == modalOverlay[_i]) {
+        modalOverlay[_i].classList.remove('show');
+      }
+    });
+  };
+
+  for (var _i = 0; _i < modalOverlay.length; _i++) {
+    _loop2(_i);
+  }
+});
--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,51 @@
-const btnBurger = document.querySelector('.menu_btn');
-const content = document.querySelector('.content');
-const articleWrap = document.querySelector('.article_wrap');
-const articleReserve = document.querySelector('.article_reserve');
-const articleMap = document.querySelector('.article_map');
-const article = document.querySelector('.article');
-const news = document.querySelector('.news');
 
-btnBurger.addEventListener('click', (event) => {
-  btnBurger.classList.toggle('menu_btn-active');
-  article.classList.toggle('article-active');
-  content.classList.toggle('content-active');  
-  articleReserve.classList.toggle('article_reserve-active');
-  news.classList.toggle('news-active');
-  event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+
+  const btnBurger = document.querySelector('.menu_btn');
+  const content = document.querySelector('.content');
+  const articleReserve = document.querySelector('.article_reserve');
+  const article = document.querySelector('.article');
+  const news = document.querySelector('.news');
+
+  btnBurger.addEventListener('click', (event) => {
+    btnBurger.classList.toggle('menu_btn-active');
+    article.classList.toggle('article-active');
+    content.classList.toggle('content-active');  
+    articleReserve.classList.toggle('article_reserve-active');
+    news.classList.toggle('news-active');
+    event.preventDefault();
+  });
+
+  const modalBtns = document.querySelectorAll("#js-open-modal");
+  const modalOverlay = document.querySelectorAll("#js-overlay-modal");
+  const modalClose = document.querySelectorAll("#js-modal-close");
+
+  for(let i = 0; i < modalBtns.length; i++) {
+    modalBtns[i].addEventListener('click', function(e) {
+      let parth = modalBtns[i].getAttribute('data-parth');
+      let modal = document.querySelector(`[data-target="${parth}"]`);
+      modal.classList.add('show');
+      e.preventDefault();
+      for(let i = 0; i < modalClose.length; i++) {
+        modalClose[i].addEventListener('click', function() {
+          modal.classList.remove('show');
+        })
+      }
+    }); 
+  }
+
+  for(let i = 0; i < modalOverlay.length; i++) {
+    modalOverlay[i].addEventListener('click', function(e) {
+      if(e.target == modalOverlay[i]) {
+        modalOverlay[i].classList.remove('show');        
+      }
+    })
+  }
+  
 });
 
 
-const btns = document.querySelectorAll("*[data-modal_btn]");
-const close = document.querySelectorAll('.news_modal_window_close');
-const modalOverlay = document.querySelectorAll('.news_modal');
 
-for(let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener('click', function(e) {
-    let name = btns[i].getAttribute('data-modal_btn');
-    let modal = document.querySelector("[data-modal_window = '"+name+"']");
-    modal.classList.add('show');
-    for(let i = 0; i < close.length; i++) {
-      close[i].addEventListener('click', function() {
-        modal.classList.remove('show');
-      });
-    }    
-  });
-}
+
+
+
